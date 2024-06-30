9a7961ddd99e609cf2e9f7a3cfdc3f68153e3050
夏沫 <summer@xiamodeMacBook-Air.local>
2024-06-30 16:53:02 +0800

    sre shell

--- sre_use/expect_ssh.sh
+++ sre_use/expect_ssh.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-USER=root
-PASS=$2
-IP=$1
-expect << EOF
-set timeout 30
-spawn ssh $USER@$IP
+#! /usr/bin/ expect
+set timeout 5
+set hostname [l index $argv 0 ]
+set password [l index $argv 1]
+spawn ssh $hostname
 expect {
-    "(yes/no)" {send "yes\r";exp_continue}
-    "password:" {send "$PASS\r"}
-}
-expect "$USER@*"  {send "$1\r"}
-expect "$USER@*"  {send "exit\r"}
-expect eof
-EOF
+"Connection refused" exit ##连接失败情况，比如对方ssh服务关闭
+"Name or service not known" exit ##找不到服务器，比如输入的IP地址不正确
+" (yes/no)" {send "yes\r" ;exp_ continue}
+"password:" {send "$password\r"}
+interact
+exit #interact后的命令不起作用
